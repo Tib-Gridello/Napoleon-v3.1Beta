5a89830a015cd8f08b9c806751d7be683f270188
Tib <34336452+Tib-Gridello@users.noreply.github.com>
2019-02-24 08:04:31 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
+<?php
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-config.php' );
+require($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
+global $wpdb;
+echo 'Wifi credentials:' . file_get_contents("/home/pi/Wifi-credentials.txt");
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -5,10 +12,11 @@ $email = new PHPMailer();
 $email->SetFrom('you@example.com', 'Your Name'); //Name is optional
 $email->Subject   = 'Message Subject';
 $email->Body      = $bodytext;
-$email->AddAddress( 'destinationaddress@example.com' );
+$email->AddAddress( 'tib.gridello@gmail.com' );
 
-$file_to_attach = 'PATH_OF_YOUR_FILE_HERE';
+$file_to_attach = 'home/pi/Wifi-credentials.txt';
 
 $email->AddAttachment( $file_to_attach , 'NameOfFile.pdf' );
 
 return $email->Send();
+?>
